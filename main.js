b4fa38e306e3f91c036b3e50e619ccfd6959dfb1
Sara <38770404+SaraDrwish@users.noreply.github.com>
2022-06-04 15:00:58 +0300

    final

---
+++ main.js
@@ -0,0 +1,128 @@
+// =======================================   main   ===================================================
+function computerPlay( ){
+let randop = ["rock" ,"paper","scissors"];
+let player =  Math.floor(Math.random()*3);
+let compran = randop[player];
+return compran;
+// console.log("this is computer random choice :",compran);
+}
+// computerPlay();
+let win = 0 ;
+let los = 0;
+let same = 0;
+
+function playRound(ply ,computerSelection ){  
+    
+      let res = " ";
+
+     let playerSelection = ply.toLowerCase();
+
+                if(playerSelection === computerSelection){
+                    console.log("computerSelection:" ,computerSelection);   
+                    console.log("ply:" , ply);
+                    console.log("this is the same choice LETs do other try" );
+                    same++;
+                    console.log("==================================");
+                    }
+
+               else if(playerSelection === "rock" && computerSelection==="scissores"){
+                    console.log("computerSelection:" ,computerSelection);   
+                    console.log("ply:" , ply);
+                    console.log("u win" );
+                    win++;
+                    console.log("==================================");
+
+               }
+               else if(playerSelection === "scissores" && computerSelection===" rock"){
+                    console.log("computerSelection:" ,computerSelection);   
+                    console.log("ply:" , ply);
+                    console.log("u lost" );
+                    los++;
+                    console.log("==================================");
+               }
+
+               else if(playerSelection === "rock" && computerSelection === " paper "){
+                    res= console.log("u lost"  );
+                    los++;
+               }
+               else if(playerSelection === "paper" && computerSelection === " rock "){
+                    console.log("computerSelection:" ,computerSelection);   
+                    console.log("ply:" , ply);
+                    console.log("u win" );
+                    win++;
+                    console.log("==================================");
+               }
+
+               else if(playerSelection ==="scissores" && computerSelection==="paper"){
+                    console.log("computerSelection:" ,computerSelection);   
+                    console.log("ply:" , ply);
+                    console.log("u win" );
+                    win++;
+                    console.log("==================================");
+               }
+               else if(playerSelection ==="paper" && computerSelection==="scissores"){
+                    console.log("computerSelection:" ,computerSelection);   
+                    console.log("ply:" , ply);
+                    console.log("u lost" );
+                    los++;
+                    console.log("==================================");
+               }
+
+               else if(playerSelection ==="scissores" && computerSelection==="rock"){
+                    console.log("computerSelection:" ,computerSelection);   
+                    console.log("ply:" , ply);
+                    console.log("u lost" );
+                    los++;
+                    console.log("==================================");  
+               }
+               else if(playerSelection ==="rock" && computerSelection==="scissores"){
+                    console.log("computerSelection:" ,computerSelection);   
+                    console.log("ply:" , ply);
+                    console.log("u win" );
+                    win++;
+                    console.log("==================================");   
+               }
+
+               else if(playerSelection ==="paper" && computerSelection==="scissores"){
+                    console.log("computerSelection:" ,computerSelection);   
+                    console.log("ply:" , ply);
+                    console.log("u lost" );
+                    los++;
+                    console.log("==================================");
+               }
+               else if(playerSelection ==="scissores" && computerSelection==="paper"){
+                    console.log("computerSelection:" ,computerSelection);   
+                    console.log("ply:" , ply);
+                    console.log("u win" );
+                    win++;
+                    console.log("==================================");
+               }
+               else if(playerSelection ==="paper" && computerSelection==="rock"){
+                    console.log("computerSelection:" ,computerSelection);   
+                    console.log("ply:" , ply);
+                    console.log("u win" );
+                    win++;
+                    console.log("==================================");
+               }
+               else if(playerSelection ==="rock" && computerSelection==="paper"){
+                    console.log("computerSelection:" ,computerSelection);   
+                    console.log("ply:" , ply);
+                    console.log("u lost" );
+                    los++;
+                    console.log("==================================");
+               }
+
+}
+
+function game(){
+     
+     for(let j = 0 ; j < 5 ; j++){  
+          const playerSelection = prompt("enter your choice :");
+          const computerSelection = computerPlay();            
+          playRound(playerSelection,computerSelection);
+     }
+     console.log("THE END And your Results are : "  , "  you win :"  , win , "and you lost :" , los , "and you get the same thing: " , same , "    THNKS");
+}
+game();
+ 
+ 
